69dff6fc13a49862ef9572596238413cd9158d1e
epicestudar <viniciusftza@gmail.com>
2023-11-17 16:58:17 -0300

    dor

--- projeto/src/app/components/header/header.component.ts
+++ projeto/src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  ImagePath: string;
+  ImageLogo: string;
+  
+  constructor() {
+    //image location
+    this.ImagePath = '/assets/img/cachorro.png',
+    this.ImageLogo = '/assets/logo/logo.png'
+  }
+  
+  ngOnInit() {
+    
+  }
 
 }
