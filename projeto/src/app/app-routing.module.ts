a3d4f3eacdd238aee17ca14ef1e1c6562de750c1
epicestudar <viniciusftza@gmail.com>
2023-11-18 20:50:48 -0300

    cards

--- projeto/src/app/app-routing.module.ts
+++ projeto/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/view/home/home.component';
 
 const routes: Routes = [
-  { path: 'header', component: HeaderComponent },
-{ path: 'footer', component: FooterComponent },
+{path: '', component: HomeComponent}
 ];
 
 @NgModule({
