bd6e797f5c6b080d667644b2733ca22a2cc6eaf3
epicestudar <viniciusftza@gmail.com>
2023-11-21 18:44:41 -0300

    link live share

--- projeto/src/app/components/view/home/home.component.ts
+++ projeto/src/app/components/view/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ImageCard1: string;
 
+  constructor() {
+     //image location
+     this.ImageCard1 = '/components/imgs/imgCards/card1.png'
+  }
+
+  ngOnInit() {
+    
+  }
 }
